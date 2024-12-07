51d1ed30ffcb915f951a8dcbe3a48b657e575122
loulou-gif <konankanjulius10@gmail.com>
2024-12-07 20:14:50 +0000

    commit web driver

--- src/test/java/com/example/Billing.java
+++ src/test/java/com/example/Billing.java
@@ -1,13 +1,14 @@
 package com.example;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import java.time.Duration;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import java.time.Duration;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Billing {
 
@@ -15,6 +16,7 @@ public class Billing {
     private WebDriverWait wait;
 
     public void setup() {
+        System.setProperty("webdriver.chrome.driver", "/Users/FRAFA-PC/automation/selenium-billing/driver/chromedriver");
         driver = new ChromeDriver();
         wait = new WebDriverWait(driver, Duration.ofSeconds(10));
     }
